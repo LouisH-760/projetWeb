dc285af1a098419a1ebe972ce26093efb5786362
Louis Hermier <louis.hermier1@etu.univ-lorraine.fr>
2020-12-01 18:50:26 +0100

    make recipe screen more comprehensive

--- afficherecette.php
+++ afficherecette.php
@@ -1,19 +1,59 @@
 <?php
+    define("EXTENSION", ".jpg");
     require_once("Donnees.inc.php");
+    require_once("bob.php");
+    // if the id isn't set, do something
+    // eventually check if the id exists?
     if(!isset($_GET["id"])) {
         // change me
         Header("Location: index.php");
     }
+    // get stuff
     $id = $_GET["id"];
+    $titre = $Recettes[$id]['titre'];
+    $ingredients = explode('|', $Recettes[$id]['ingredients']);
+    $préparation = $Recettes[$id]['preparation'];
+    $tags = array();
+    foreach($Recettes[$id]['index'] as $elem) {
+        $tags[] = '<a href="index.php' . buildGetParams('params', getTree($elem, $Hierarchie)) . '">' . $elem . '</a>';
+    }
+
+    $image = "Photos/" . normalizeText($titre) . EXTENSION;
+    $imgContainer = (file_exists($image)) ? '<div class="image"><p><h2>Image</h2><img src="' . $image . '" alt="' . $titre . '" /></p></div>' : NULL;
 ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recette</title>
+    <title><?php echo $titre; ?></title>
 </head>
 <body>
-    <?php print_r($Recettes[$id]); ?>
+    <p>
+        <h1><?php echo $titre; ?></h1>
+        <h2>Ingrédients</h2>
+        <ul class="ingrédients">
+            <?php
+                foreach($ingredients as $ingredient) {
+                    echo '<li>' . $ingredient . '</li>';
+                }
+            ?>
+        </ul>
+    </p>
+    <p>
+        <h2>Instructions</h2>
+        <?php echo $préparation; ?>
+    </p>
+    <p>
+        <?php echo $imgContainer; ?>
+    </p>
+    <footer>
+        <h2>Tags</h2>
+        <p>
+            <?php
+                echo implode(", ", $tags);
+            ?>
+        </p>
+    </footer>
 </body>
 </html>
