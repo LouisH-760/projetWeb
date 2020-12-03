5fa513a6b1bdadeb5cab7d153810f08c0f7b16e3
Louis Hermier <louis.hermier1@etu.univ-lorraine.fr>
2020-12-03 21:42:18 +0100

    add styling to registration form

--- FirstLogin.php
+++ FirstLogin.php
@@ -1,52 +1,58 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 
 <head>
     <title></title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="style/register.css">
 </head>
 
 <body>
 
-    <h1></h1>
+    <nav>
+        <a href="index.php" id="mainlink">Retour à l'accueil</a>
+    </nav>
+    <main>
+        <h1>Créer un profil utilisateur</h1>
 
-    <form method="post" action="Verif.php">
-        <fieldset>
-            <legend>Informations</legend>
+        <form method="post" action="Verif.php">
+            <fieldset>
+                <legend>Informations Personelles</legend>
 
-            Login :
-            <input type="text" name="login" required="required" /><br />
-            Mot de passe :
-            <input type="text" name="passe" required="required" /><br />
-            Nom :
-            <input type="text" name="nom" /><br />
-            Prénom :
-            <input type="text" name="prenom" /><br />
-            Vous êtes :
-            <label><input type="radio" name="sexe" value="f" /> femme</label>
-            <label><input type="radio" name="sexe" value="h" /> homme</label>
-            <br />
-            Mail :
-            <input type="text" name="mail" /><br />
-            Date de naissance :
-            <input type="date" name="naissance" /><br />
-            Adresse :
-            <input type="text" name="adresse" /><br />
-            Adresse CP :
-            <input type="text" name="adresseCP" /><br />
-            Adresse Ville :
-            <input type="text" name="adresseV" /><br />
-
-
-        </fieldset>
-
-
-        <br />
-        <input type="submit" value="Valider" />
-
-    </form>
+                Login :
+                <input type="text" name="login" required="required" class="fullwidth" /><br />
+                Mot de passe :
+                <input type="text" name="passe" required="required" class="fullwidth" /><br />
+                Nom :
+                <input type="text" name="nom" class="fullwidth" /><br />
+                Prénom :
+                <input type="text" name="prenom" class="fullwidth" /><br />
+                Vous êtes :
+                <label><input type="radio" name="sexe" value="f" /> femme</label>
+                <label><input type="radio" name="sexe" value="h" /> homme</label>
+                <br />
+                Mail :
+                <input type="text" name="mail" class="fullwidth" /><br />
+                Date de naissance :
+                <input type="date" name="naissance" class="fullwidth" /><br />
+                Adresse :
+                <input type="text" name="adresse" class="fullwidth" /><br />
+                <div class="fullwidth">
+                    <div class="cpwidth">
+                        Code Postal :
+                        <input type="text" name="adresseCP" class="fullwidth"/>
+                    </div>
+                    <div class="vwidth">
+                        Ville :
+                        <input type="text" name="adresseV" class="fullwidth" /><br />
+                    </div>
+                </div>
+            </fieldset>
+            <input type="submit" value="Valider" />
+        </form>
+    </main>
 </body>
 
 </html>
