704b1200cbc00f62c650dbead0f75d40c20f8d65
JulianK <juliankrug123@gmail.com>
2020-12-03 18:06:45 +0100

    add login

---
+++ Verif.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+      <title>Vérification</title>
+	  <meta charset="utf-8" />
+</head>
+<body>
+<?php 
+  print_r($_POST); 
+?>
+<br>
+Login : <?php 
+  if(isset($_POST['login'])) { 
+    $login=trim($_POST['login']);
+    		echo $login;
+    }
+?>
+<br>
+  Mot de passe : <?php 
+  if(isset($_POST['passe']))  { 
+        $passe=strtolower(trim($_POST['passe']));  
+        echo $passe;
+  }
+?>
+</body>
+</html>
