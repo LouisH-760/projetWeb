5ecd89267aa174b1fa2d98915e1a59110d5ba41d
Louis Hermier <louis.hermier1@etu.univ-lorraine.fr>
2020-12-03 20:57:18 +0100

    add comments to the javascript code

--- search.js
+++ search.js
@@ -1,4 +1,7 @@
+// regex object with both separators to do it all at once
 const SPLIT = /([-\+])/g;
+// both our separators for later comparison.
+// if you change one, change the regex too!
 const PLUSSEP = "+";
 const MINUSSEP = "-";
 
@@ -6,26 +9,39 @@ const MINUSSEP = "-";
  * Retrieve the search string, parse it.
  */
 function handle() {
+    // searchbar trimmed value
     let inputVal = $('#searchbar').val().trim();
+    // get the arguments array
     let toSend = parseArgs(inputVal);
-    // replace this by sending to the backend
+    // rTODO eplace this by sending to the backend
     console.log(toSend);
-    // replace placeholder with actual autocomplete to display
+    // TODO replace placeholder with actual autocomplete to display
     addToAutoCompleteBox(toSend.include);
 }
 
+/**
+ * Take an array of strings or whatever as input,
+ * then add them as autocomplete prompts to the searchbar
+ * @param {array} array 
+ */
 function addToAutoCompleteBox(array) {
     // remove all existing autocomplete items
     $('.autocomplete-items').remove();
+    // create the container
     let a = document.createElement("DIV");
     a.setAttribute("class", "autocomplete-items");
+    // attach it to the searchbar
     document.getElementById("searchbar").parentNode.appendChild(a);
     for (let val of array) {
+        // create individual autocompletes
         let b = document.createElement("DIV");
+        // set the value
         b.innerHTML = val;
+        // make it so if one is clicked, its value is appended to the searchbar
         b.addEventListener("click", function (e) {
             $('#searchbar').val($('#searchbar').val() + this.innerHTML);
         });
+        // append it to the container
         a.appendChild(b);
     }
 }
@@ -34,13 +50,15 @@ function addToAutoCompleteBox(array) {
  * Split the search query in an object.
  * Everything after a + is an exact string to match / until the next +/-
  * Everything after a - is an exact string to not match / until the next +/-
- * @param {*} query : query to search from
+ * @param {string} query : query to search from
  */
 function parseArgs(query) {
     let tmpplus = [];
     let tmpminus = [];
     let tmpquery = [];
     let firstSplit = query.split(SPLIT);
+    // go through the list. The separators (+ & -) are on their own, followed by their content
+    // useful to split everything into a nice object!
     for (let i = 0; i < firstSplit.length; i++) {
         if (firstSplit[i] == PLUSSEP) {
             tmpplus.push(firstSplit[i + 1].trim());
@@ -55,7 +73,12 @@ function parseArgs(query) {
     return { "query": tmpquery, "include": tmpplus, "exclude": tmpminus };
 }
 
+// if the script is properly referenced in the head (with "defer"),
+// then this won't run before the DOM finished loading.
+// trigger autocompletion on key release and focus gained from the searchbar
 $("#searchbar").keyup(handle);
-document.addEventListener("click", function(e) {
+$("#searchbar").onfocus(handle);
+// non-jQuery listen to encompass the whole DOM
+document.addEventListener("click", function (e) {
     $('.autocomplete-items').remove();
 });
